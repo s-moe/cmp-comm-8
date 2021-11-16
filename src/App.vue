588fe2b1cc76389819547dd3d6c3612c22b9e006
Sarah Moe <sarahannmoe@gmail.com>
2021-11-16 08:15:41 -0800

    remove emits

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       :topic-title="activeTopic && activeTopic.title"
       :text="activeTopic && activeTopic.fullText"
     ></active-element>
-    <knowledge-base @select-topic="activateTopic"></knowledge-base>
+    <knowledge-base></knowledge-base>
   </div>
 </template>
 
